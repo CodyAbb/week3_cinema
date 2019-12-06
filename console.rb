25ebe7447fa235ecc6a3ef507c9a87e63fd67077
Cody Abbott <cody.abbott16@gmail.com>
2019-12-06 14:24:32 +0000

    finished up save and delete for all classes

--- console.rb
+++ console.rb
@@ -3,16 +3,67 @@ require_relative('models/customer')
 require_relative('models/film')
 require_relative('models/ticket')
 
-# Ticket.delete_all()
+Ticket.delete_all()
 Film.delete_all()
-# Customer.delete_all()
+Customer.delete_all()
 
 film1 = Film.new({
-  'name' => "Die Hard",
+  'title' => "Die Hard",
   'price' => 5
   })
 
 film1.save()
+
+film2 = Film.new({
+  'title' => "White Christmas",
+  'price' => 10
+  })
+
+film2.save()
+
+
+customer1 = Customer.new({
+  'name' => "Joe",
+  'funds' => "50"
+  })
+customer1.save()
+
+customer2 = Customer.new({
+  'name' => "Rebecca",
+  'funds' => "20"
+  })
+customer2.save()
+
+customer3 = Customer.new({
+  'name' => "Victoria",
+  'funds' => "80"
+  })
+customer3.save()
+
+
+ticket1 = Ticket.new({
+  'film_id' => film1.id,
+  'customer_id' => customer1.id
+  })
+ticket1.save()
+
+ticket2 = Ticket.new({
+  'film_id' => film1.id,
+  'customer_id' => customer2.id
+  })
+ticket2.save()
+
+ticket3 = Ticket.new({
+  'film_id' => film2.id,
+  'customer_id' => customer3.id
+  })
+ticket3.save()
+
+ticket4 = Ticket.new({
+  'film_id' => film2.id,
+  'customer_id' => customer2.id
+  })
+ticket4.save()
 
 binding.pry
 nil
---
+++ models/customer.rb
@@ -0,0 +1,26 @@
+require_relative('../db/psql_runner')
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds']
+  end
+
+  def save()
+    sql = "INSERT INTO customers (name, funds) VALUES ($1, $2) RETURNING id"
+    values = [@name, @funds]
+    customer = SqlRunner.run(sql, values).first
+    @id = customer['id'].to_i
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM customers"
+    SqlRunner.run(sql)
+  end
+
+end
