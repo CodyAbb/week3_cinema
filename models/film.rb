f8cd72033a42f2b0acb42bb3e9b5b60e7299af38
Cody Abbott <cody.abbott16@gmail.com>
2019-12-06 14:44:29 +0000

    added update functionality classes

--- models/film.rb
+++ models/film.rb
@@ -24,6 +24,12 @@ class Film
     SqlRunner.run(sql, values)
   end
 
+  def update()
+   sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
+   values = [@title, @price, @id]
+   SqlRunner.run(sql, values)
+  end
+
   def self.all()
     sql = "SELECT * FROM films"
     film_data = SqlRunner.run(sql)
